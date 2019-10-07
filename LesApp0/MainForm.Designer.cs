c5d3402d4f02f1e4ceca3022e27861d1422e244c
Bogdan <pinchuk.brus@gmail.com>
2019-10-07 11:45:01 +0300

    Add LesApp0

---
+++ LesApp0/MainForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace LesApp0
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ms = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolLb = new System.Windows.Forms.ToolStripLabel();
+            this.tb = new System.Windows.Forms.TextBox();
+            this.openFD = new System.Windows.Forms.OpenFileDialog();
+            this.analysis = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.ms.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ms
+            // 
+            this.ms.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.analysis});
+            this.ms.Location = new System.Drawing.Point(0, 0);
+            this.ms.Name = "ms";
+            this.ms.Size = new System.Drawing.Size(479, 24);
+            this.ms.TabIndex = 0;
+            this.ms.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFile,
+            this.toolStripSeparator1,
+            this.toolStripMenuItem1});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFile
+            // 
+            this.openFile.Name = "openFile";
+            this.openFile.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openFile.Size = new System.Drawing.Size(152, 22);
+            this.openFile.Text = "Open";
+            this.openFile.ToolTipText = "Open file *.exe, *.dll";
+            this.openFile.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem1.Text = "Close";
+            this.toolStripMenuItem1.ToolTipText = "Close program";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.ToolStripMenuItem1_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolLb});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 284);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(479, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolLb
+            // 
+            this.toolLb.Name = "toolLb";
+            this.toolLb.Size = new System.Drawing.Size(70, 22);
+            this.toolLb.Text = "Information";
+            // 
+            // tb
+            // 
+            this.tb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tb.Location = new System.Drawing.Point(0, 24);
+            this.tb.Multiline = true;
+            this.tb.Name = "tb";
+            this.tb.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tb.Size = new System.Drawing.Size(479, 260);
+            this.tb.TabIndex = 2;
+            // 
+            // openFD
+            // 
+            this.openFD.FileName = "openFD";
+            this.openFD.Filter = "Programs *.exe|*.exe|Library *.dll|*.dll|All Files|*.*";
+            // 
+            // analysis
+            // 
+            this.analysis.Enabled = false;
+            this.analysis.Name = "analysis";
+            this.analysis.Size = new System.Drawing.Size(62, 20);
+            this.analysis.Text = "Analysis";
+            this.analysis.ToolTipText = "Analysis loaded file";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 309);
+            this.Controls.Add(this.tb);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.ms);
+            this.MainMenuStrip = this.ms;
+            this.Name = "Form1";
+            this.Text = "LesApp0";
+            this.ms.ResumeLayout(false);
+            this.ms.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip ms;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFile;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolLb;
+        private System.Windows.Forms.TextBox tb;
+        private System.Windows.Forms.OpenFileDialog openFD;
+        private System.Windows.Forms.ToolStripMenuItem analysis;
+        private System.Windows.Forms.ToolTip toolTip;
+    }
+}
+
---
+++ LesApp0/MainForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LesApp0
+{
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// Шлях до файлу
+        /// </summary>
+        private string path = string.Empty;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Вихід із програми
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ToolStripMenuItem1_Click(object sender, EventArgs e)
+            => this.Close();
+
+        /// <summary>
+        /// Відкриття файла аналізу
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void openFile_Click(object sender, EventArgs e)
+        {
+            if (openFD.ShowDialog() == DialogResult.OK)
+            {
+                // присвоєння шляху/повної назви файла
+                path = openFD.FileName;
+
+                if (!(Path.GetExtension(path) == ".exe" ||
+                    Path.GetExtension(path) == ".dll"))
+                {
+                    // якщо завантажено файл але не відповідного розширення
+                    toolLb.Text = "File loaded unsuccessfully.";
+                    path = string.Empty;
+                    analysis.Enabled = false;
+                    return;
+                }
+
+                // якщо завантажено файл з правильним розширенням
+                toolLb.Text = "File loaded successfully.";
+                analysis.Enabled = true;
+            }
+            else
+            {
+                // якщо натиснута відміна
+                toolLb.Text = "File wasn't loaded.";
+            }
+        }
+    }
+}
